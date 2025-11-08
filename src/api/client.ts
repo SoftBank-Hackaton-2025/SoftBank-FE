759a1b32b5efd57cb6d4a7e6134bf282b3dbda4d
HOJEONGKIMM <khj15263245@gmail.com>
2025-11-09 01:00:35 +0900

    Fix: Fix time limit

--- src/api/client.ts
+++ src/api/client.ts
@@ -7,7 +7,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 60000,
+  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
   },
